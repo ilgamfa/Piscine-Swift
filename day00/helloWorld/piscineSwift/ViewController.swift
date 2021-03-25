1648bcca7989a47da0cabe5da34054fccfffcc00
Ильгам Ахматдинов <ilgamfa@MacBook-Pro.local>
2021-03-25 20:59:08 +0300

    add day00 dir

---
+++ day00/helloWorld/piscineSwift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  piscineSwift
+//
+//  Created by Ильгам Ахматдинов on 25.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickBtn(_ sender: UIButton) {
+        print("Hello world !")
+    }
+    
+}
+
