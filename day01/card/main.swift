a58717c76ac03fa0bf3b5e2c9db01587e68fa4ba
Ильгам Ахматдинов <ilgamfa@yandex.ru>
2021-03-29 00:24:09 +0300

    new day

---
+++ day01/card/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  colorAndValue
+//
+//  Created by Ильгам Ахматдинов on 28.03.2021.
+//
+
+var arrColor: [Color] = Color.allColors
+var arrValue: [Value] = Value.allValues
+
+//for elem in arrValue {
+//   print("\(elem) = \(elem.rawValue)")
+//}
+
+for elem in arrValue {
+    print("\(elem) = \(elem.rawValue)")
+}
