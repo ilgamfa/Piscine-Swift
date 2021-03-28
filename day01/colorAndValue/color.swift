544daac16f66a9d464d32e7e861be1cd595f5ffe
Ильгам Ахматдинов <ilgamfa@yandex.ru>
2021-03-28 23:47:49 +0300

    color and value

---
+++ day01/colorAndValue/color.swift
@@ -0,0 +1,17 @@
+//
+//  color.swift
+//  colorAndValue
+//
+//  Created by Ильгам Ахматдинов on 28.03.2021.
+//
+
+import Foundation
+
+public enum Color: String {
+    case Red
+    case Green
+    case Blue
+    case White
+    
+    static let allColors: [Color] = [Red, Green, Blue, White]
+}
