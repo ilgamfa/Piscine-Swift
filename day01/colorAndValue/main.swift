fac2a75d0bfbfe411954c034476bf61834754695
Ильгам Ахматдинов <ilgamfa@yandex.ru>
2021-03-29 23:31:18 +0300

    add day03

--- day01/colorAndValue/main.swift
+++ day01/colorAndValue/main.swift
@@ -8,9 +8,9 @@
 var arrColor: [Color] = Color.allColors
 var arrValue: [Value] = Value.allValues
 
-for elem in arrValue {
-    print("\(elem) = \(elem.rawValue)")
-}
+//for elem in arrValue {
+//   print("\(elem) = \(elem.rawValue)")
+//}
 
 for elem in arrValue {
     print("\(elem) = \(elem.rawValue)")
